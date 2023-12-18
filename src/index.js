8943f7b532d22216db16c3e36ccbe4958a1f1ca5
Damu-01 <damuingle@gmail.com>
2023-12-18 16:39:40 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import {React, lazy,Suspense }from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import Body from './components/Body';
 import reportWebVitals from './reportWebVitals';
+const Project = lazy(()=>import ('./components/Projects'))
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+     
+      {
+        path: "/projects",
+        element: <Suspense> <Project /> </Suspense>
+      },
+     
+    ]
+  }
+])
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
